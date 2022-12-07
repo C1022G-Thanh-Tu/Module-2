cd92a81037ca6110092fe839154c1189ac7ce933
thanhtu2607 <nthtu1998@gmail.com>
2022-12-07 14:06:17 +0700

    update bai tap

--- src/ss4_class_object_java/QuadraticEquation.java
+++ src/ss4_class_object_java/QuadraticEquation.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class QuadraticEquation {
     private double a, b, c;
-    public double delta, r1, r2;
 
     public QuadraticEquation(double a, double b, double c) {
         this.a = a;
