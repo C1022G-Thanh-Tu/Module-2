8e0c5e0368facaf4e5b11fa90722b139372e000a
thanhtu2607 <nthtu1998@gmail.com>
2022-12-07 20:17:44 +0700

    update

--- src/giai_thuat/ArrayFibonacciPrimes.java
+++ src/giai_thuat/ArrayFibonacciPrimes.java
@@ -21,34 +21,22 @@ public class ArrayFibonacciPrimes {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Nhập số nguyên");
         int numbInput = Integer.parseInt(scanner.nextLine());
-        int[] arr1 = new int[numbInput+1];
-        int[] arr2 = new int[numbInput+1];
-        arr1[0] = 0;
-        arr1[1] = 1;
-        arr1[2] = arr1[0] + arr1[1];
-        int i,j;
-        for (i = 3; i<arr1.length; i++) {
-            arr1[i] = arr1[i-1] + arr1[i-2];
-        }
-        for (i=0; i<arr1.length; i++) {
-            if (isPrime(arr1[i])) {
-                arr2[i] = arr1[i];
+        int[] arr = new int[numbInput];
+        int n1 = 0;
+        int n2 = 1;
+        int nTotal = n1 + n2;
+        int count = 0;
+        int i = 0;
+        while (count<numbInput) {
+            n1= n2;
+            n2 = nTotal;
+            nTotal = n1 + n2;
+            if (isPrime(nTotal)) {
+                arr[i] = nTotal;
+                i++;
+                count++;
             }
         }
-        for (i=0; i<arr2.length; i++) {
-            if (arr2[i] == 0) {
-                for (j=i; j<arr2.length-1; j++) {
-                    arr2[j] = arr2[j+1];
-                }
-                arr2[arr2.length-1] = 1;
-                i--;
-            }
-        }
-        for (i=0; i<arr2.length; i++) {
-            if (arr2[i] == 1) {
-                arr2[i] = 0;
-            }
-        }
-        System.out.println(Arrays.toString(arr2));
+        System.out.println(Arrays.toString(arr));
     }
 }
