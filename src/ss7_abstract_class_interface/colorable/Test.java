395ab14a6e57c75fefbee6ee00863e3458302c83
thanhtu2607 <nthtu1998@gmail.com>
2022-12-13 14:50:08 +0700

    update

--- src/ss7_abstract_class_interface/colorable/Test.java
+++ src/ss7_abstract_class_interface/colorable/Test.java
@@ -6,13 +6,13 @@ public class Test {
         shapeArr[0] = new Square("red", true, 5.6, "Hình vuông");
         shapeArr[1] = new Circle("yellow", false, 7.8, "Hình tròn");
         shapeArr[2] = new Rectangle("black", false, 3.6, 9.7, "Hình chữ nhật");
-        for (int i = 0; i < shapeArr.length; i++) {
-            if (shapeArr[i] instanceof Square) {
+        for (Shape shape : shapeArr) {
+            if (shape instanceof Square) {
                 Square square = (Square) shapeArr[0];
                 square.howToCoLor();
                 square.getArea();
                 System.out.println();
-            } else if (shapeArr[i] instanceof Circle) {
+            } else if (shape instanceof Circle) {
                 Circle circle = (Circle) shapeArr[1];
                 circle.getArea();
                 System.out.println();
