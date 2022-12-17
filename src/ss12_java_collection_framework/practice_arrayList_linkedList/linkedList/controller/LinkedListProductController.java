19ea5117700c7c43e4fd292659b19de2fc2dd8cd
thanhtu2607 <nthtu1998@gmail.com>
2022-12-17 07:45:19 +0700

    update

--- src/ss12_java_collection_framework/practice_arrayList_linkedList/linkedList/controller/LinkedListProductController.java
+++ src/ss12_java_collection_framework/practice_arrayList_linkedList/linkedList/controller/LinkedListProductController.java
@@ -10,15 +10,15 @@ public class LinkedListProductController {
     private static final IArrayListProductManagerService productService = new ArrayListProductManagerService();
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("-----Product Manager-------");
+        System.out.println("1. Thêm sản phẩm");
+        System.out.println("2. Điều chỉnh sản phẩm theo id");
+        System.out.println("3. Xóa sản phẩm");
+        System.out.println("4. Lấy danh sách sản phẩm");
+        System.out.println("5. Tìm sản phẩm theo tên");
+        System.out.println("6. Sắp xếp sản phẩm theo thứ tự giá tăng dần");
+        System.out.println("7. Sắp xếp sản phẩm theo thứ tự giá giảm dần");
         do {
-            System.out.println("-----Product Manager-------");
-            System.out.println("1. Thêm sản phẩm");
-            System.out.println("2. Điều chỉnh sản phẩm theo id");
-            System.out.println("3. Xóa sản phẩm");
-            System.out.println("4. Lấy danh sách sản phẩm");
-            System.out.println("5. Tìm sản phẩm theo tên");
-            System.out.println("6. Sắp xếp sản phẩm theo thứ tự giá tăng dần");
-            System.out.println("7. Sắp xếp sản phẩm theo thứ tự giá giảm dần");
             System.out.print("Hãy chọn một dịch vụ: ");
             int select = Integer.parseInt(scanner.nextLine());
             switch (select) {
