7bc812ce0ff514b432f98b4ba819cdd60448faa8
thanhtu2607 <nthtu1998@gmail.com>
2022-12-20 13:04:01 +0700

    update

--- src/ss14_sorting_algorithms/InsertionSortDescription.java
+++ src/ss14_sorting_algorithms/InsertionSortDescription.java
@@ -1,8 +1,18 @@
 package ss14_sorting_algorithms;
 
+import java.util.Arrays;
+
 public class InsertionSortDescription {
     public static void main(String[] args) {
         int[] arr = {4,2,8,19,12,56,89};
-
+        for (int i = 1; i < arr.length; i++) {
+            int j = i;
+            for (;j>0 && arr[j-1]>arr[j]; j--) {
+                int temp = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = temp;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
--- src/ss14_sorting_algorithms/InsertionSortSetting.java
+++ src/ss14_sorting_algorithms/InsertionSortSetting.java
@@ -1,4 +1,21 @@
 package ss14_sorting_algorithms;
 
+import java.util.Arrays;
+
 public class InsertionSortSetting {
+    public static int[] insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int j = i;
+            for (;j>0 && arr[j-1]>arr[j]; j--) {
+                int temp = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = temp;
+            }
+        }
+        return arr;
+    }
+    public static void main(String[] args) {
+        int[] array = {4,2,8,19,12,56,89};
+        System.out.println(Arrays.toString(insertionSort(array)));
+    }
 }
