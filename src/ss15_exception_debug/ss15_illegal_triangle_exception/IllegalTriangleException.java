6f539a14dcc3462c680eb94f7f3eeaae200c9bbf
thanhtu2607 <nthtu1998@gmail.com>
2022-12-21 10:54:10 +0700

    update

--- src/ss15_exception_debug/ss15_illegal_triangle_exception/IllegalTriangleException.java
+++ src/ss15_exception_debug/ss15_illegal_triangle_exception/IllegalTriangleException.java
@@ -1,6 +1,3 @@
 package ss15_exception_debug.ss15_illegal_triangle_exception;
 
-public class IllegalTriangleException extends Exception{
-    public IllegalTriangleException() {
-    }
-}
+public class IllegalTriangleException extends Exception{}
