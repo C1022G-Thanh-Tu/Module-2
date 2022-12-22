2ba651e1cbaf2cd6e42ad469d6f97bd838dc7834
thanhtu2607 <nthtu1998@gmail.com>
2022-12-22 21:48:43 +0700

    update

--- src/demo/Test.java
+++ src/demo/Test.java
@@ -3,39 +3,46 @@ package demo;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Test {
 
     public static final String FILE_NAME = "D:\\CODEGYM\\Module2\\C1022G1\\src\\demo\\Demo.csv";
+    public static final String TEST_CSV = "D:\\CODEGYM\\Module2\\C1022G1\\src\\demo\\Test.csv";
 
-    public static List<Demo> readCSV() throws IOException {
-        List<Demo> demoList = new ArrayList<>();
+    public static List<String> readCSV() throws IOException {
+        List<String> demoList = new ArrayList<>();
         FileReader fileReader = new FileReader(FILE_NAME);
         BufferedReader bufferedReader = new BufferedReader(fileReader);
         String line;
-        String[] temp;
-        Demo demo;
+//        String[] temp;
+//        Demo demo;
         while ((line = bufferedReader.readLine()) != null) {
-            temp = line.split(",");
-            int id = Integer.parseInt(temp[0]);
-            String name = temp[1];
-            demo = new Demo(id, name);
-            demoList.add(demo);
+//            temp = line.split(",");
+//            int id = Integer.parseInt(temp[0]);
+//            String name = temp[1];
+//            demo = new Demo(id, name);
+            demoList.add(line);
         }
         return demoList;
     }
-    public static void writeCSV(Demo demo) throws IOException {
-        FileWriter fileWriter = new FileWriter(FILE_NAME, true);
-        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-        bufferedWriter.write("\n" + demo.getId() + "," + demo.getName());
-        bufferedWriter.close();
-    }
+//    public static void writeCSV(Demo demo) throws IOException {
+//        FileWriter fileWriter = new FileWriter(FILE_NAME, true);
+//        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+//        bufferedWriter.write("\n" + demo.getId() + "," + demo.getName());
+//        bufferedWriter.close();
+//    }
     public static void main(String [] args) throws IOException {
-//        List<Demo> demoList = readCSV();
+        List<String> demoList = readCSV();
+        FileWriter fileWriter = new FileWriter(TEST_CSV);
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        for (String s : demoList) {
+            bufferedWriter.write(s);
+            bufferedWriter.newLine();
+        }
+        bufferedWriter.flush();
 //        for (Demo demo : demoList) {
 //            System.out.println(demo);
 //        }
-        writeCSV(new Demo(4, "d"));
+//        writeCSV(new Demo(4, "d"));
     }
 }
--- src/ss16_io_text_file/copy_file_text/FileUtil.java
+++ src/ss16_io_text_file/copy_file_text/FileUtil.java
@@ -1,12 +1,15 @@
 package ss16_io_text_file.copy_file_text;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FileUtil {
     public static final String FILE_NAME1 = "D:\\CODEGYM\\Module2\\C1022G1\\src\\ss16_io_text_file\\copy_file_text\\source_file.csv";
     public static final String FILE_NAME2 = "D:\\CODEGYM\\Module2\\C1022G1\\src\\ss16_io_text_file\\copy_file_text\\target_file.csv";
 
-    public static void readAndWriteCSV() {
+    public static List<String> readAndWriteCSV() {
+        List<String> listArr = new ArrayList<>();
         FileReader fileReader = null;
         BufferedReader bufferedReader = null;
         String line;
@@ -20,22 +23,11 @@ public class FileUtil {
             e.printStackTrace();
         }
 
-        FileWriter fileWriter = null;
-        BufferedWriter bufferedWriter = null;
-        try {
-            fileWriter = new FileWriter(FILE_NAME2);
-            bufferedWriter = new BufferedWriter(fileWriter);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
         try {
             while ((line = bufferedReader.readLine()) != null) {
-                bufferedWriter.write(line);
-                bufferedWriter.newLine();
-                count+=line.length();
+                listArr.add(line);
+                count += line.length();
             }
-            bufferedWriter.flush();
             System.out.println("Số các ký tự đã sao chép là: " + count);
         } catch (IOException e) {
             e.printStackTrace();
@@ -46,9 +38,27 @@ public class FileUtil {
                 e.printStackTrace();
             }
         }
+        return listArr;
     }
 
     public static void main(String[] args) {
-        readAndWriteCSV();
+        List<String> stringList = readAndWriteCSV();
+        FileWriter fileWriter = null;
+        BufferedWriter bufferedWriter = null;
+        try {
+            fileWriter = new FileWriter(FILE_NAME2);
+            bufferedWriter = new BufferedWriter(fileWriter);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            for (String str : stringList) {
+                bufferedWriter.write(str);
+                bufferedWriter.newLine();
+            }
+            bufferedWriter.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
